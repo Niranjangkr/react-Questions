a03adca07dc059e33646b915c5f9deac3b93a181
Niranjan Gaonkar <niranjangaonkar.09@gmail.com>
2023-08-03 10:28:15 +0530

    first problem

---
+++ Practice/src/components/Form.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+import Strength from './Strength';
+
+function Form() {
+    const [ text, setText ] = useState('');
+    const [ hide, setHide ] = useState(false)
+    let mask = '';
+
+    for(let i =0; i<text.length; i++){
+        mask += '*';
+    }
+    console.log(text)
+
+  return (
+    <>
+     <input type={hide?'password':'text'} name="" id="" onChange = {(e) => setText(e.target.value)} value={text}/>
+     <button onClick={() => setHide(prevState => !prevState)}>{hide?'Show':'Hide'}</button>
+     <button onClick={() => setText('')}>Clear All</button>
+     <Strength />   
+    </>
+  )
+}
+
+export default Form
---
+++ Practice/src/components/Strength.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+// strength calculate  :condition 
+// -upperCase 
+// -lowerCase 
+// -special Character "$#_" 
+// -length >=8
+// -number
+
+// if s < 3 == weak -red color
+// if s == 3 || 4 moderate with orange color
+// else strong with green color
+
+const Strength = () => {
+  return (
+    <div>Strength</div>
+  )
+}
+
+export default Strength
